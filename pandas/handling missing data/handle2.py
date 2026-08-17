5480d1fba2bf6a864354c37d9d869b2908be5b6e
Kailash <Kp8347@gmail.com>
2026-08-17 21:20:46 +0530

    pandas ok

--- pandas/handling missing data/handle2.py
+++ pandas/handling missing data/handle2.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 data = {
-    "name": ["Rahul", None, "Amit", "Neha", "Karan", "Riya", "Vivek", "Anjali", "Rohit", "Pooja"],
+    "name": ["Rahul", None, "Amit", "Neha", None, "Riya", "Vivek", "Anjali", "Rohit", "Pooja"],
     "age": [25, None, 24, 30, 27, 26, 32, 29, 31, 23],
     "salary": [35000, None, 32000, 55000, 42000, 38000, 60000, 48000, 52000, 30000],
-    "performancescore": [82, 55, 75, 88, 79, 85, 95, 89, 84, 72]
+    "performancescore": [82, None, 75, 88, 79, 85, 95, 89, 84, 72]
 }
 
 df = pd.DataFrame(data)
@@ -14,5 +14,5 @@ print(df)
 df["name"]=df["name"].fillna("Unknown")
 df["age"]=df["age"].fillna(df["age"].mean())
 df["salary"]=df["salary"].fillna(df["salary"].mean())
-
+df["performancescore"]=df["performancescore"].fillna(df["performancescore"].max())
 print(df)
